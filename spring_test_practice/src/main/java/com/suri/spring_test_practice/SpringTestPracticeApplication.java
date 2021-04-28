d2584661d3e95c8e9ff2b723c2c341add1948852
dwl5 <ssop6403@gmail.com>
2021-04-28 16:58:47 +0900

    initial source commit

---
+++ spring_test_practice/src/main/java/com/suri/spring_test_practice/SpringTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.suri.spring_test_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestPracticeApplication.class, args);
+	}
+
+}
---
+++ spring_test_practice/src/test/java/com/suri/spring_test_practice/SpringTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suri.spring_test_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
